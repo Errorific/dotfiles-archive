96389e0ef796b972a054fb238962ebafc185abb2
Christopher Mckay <cmckay@iseek.com.au>
2013-04-08 17:05:45 +1000

    more emacs magic

--- emacs/init.el
+++ emacs/init.el
@@ -4,6 +4,7 @@
   (mouse-wheel-mode t)
   (blink-cursor-mode -1))
 
+;;{{{ el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
@@ -20,10 +21,15 @@
 
 (setq my-packages
      (append
-      '(el-get cperl-mode)
+      '(el-get cperl-mode scala-mode zenburn-theme coffee-mode)
       (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get-cleanup my-packages)
 (el-get 'sync my-packages)
+;;}}}
 
+(make-directory "~/.emacs.d/autosaves/" t)
+(make-directory "~/.emacs.d/backups/" t)
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "autosaves") t)))
+(setq backup-by-copying t)
