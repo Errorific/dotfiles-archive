7f470711f25a1d2b84b5b61057f0edbbbf5af2e1
Christopher Mckay <error@errorific.com>
2013-12-29 17:35:21 +1000

    fixing emacs cursor colours

--- emacs/init.el
+++ emacs/init.el
@@ -58,6 +58,7 @@
 ;;}}}
 
 (evil-mode 1)
+(setq evil-default-cursor t)
 
 (add-hook 'prog-mode-hook 'highlight-parentheses-mode)
 
@@ -115,17 +116,3 @@
 (global-whitespace-mode t)
 ;;}}}
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;;'(sp-pair-overlay-face ((t nil)))
- ;;'(sp-show-pair-match-face ((t (:background "SteelBlue4")))))
-)
